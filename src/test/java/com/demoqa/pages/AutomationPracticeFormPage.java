0ea9a70a5732d8782e8ed640f731c5d937af0873
Nadezhda <124467288+PervukhinaNadezhda@users.noreply.github.com>
2023-06-14 23:13:00 +0300

    Page objects homework (#2)
    * Page Objects homework
    
    * Page Objects homework. Divided openPage() and add RemoveBanner() in AutomationPracticeFormPage +

---
+++ src/test/java/com/demoqa/pages/AutomationPracticeFormPage.java
@@ -0,0 +1,116 @@
+package com.demoqa.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import com.demoqa.pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AutomationPracticeFormPage {
+    CalendarComponent calendarComponent = new CalendarComponent();
+    SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            gender = $("#genterWrapper"),
+            userNumberInput = $("#userNumber"),
+            birthDayInput = $("#dateOfBirth-wrapper").$("#dateOfBirthInput"),
+            subjectValueInput = $("#subjectsInput"),
+            subjectChosen = $("#subjectsWrapper"),
+            hobbyInput = $("#hobbiesWrapper"),
+            uploadPictureInput = $("#uploadPicture"),
+            addressInput = $("#currentAddress"),
+            stateInput = $("#state"),
+            stateChosen = $("#stateCity-wrapper"),
+            cityInput = $("#city"),
+            cityChosen = $("#stateCity-wrapper"),
+            submitButton = $("#submit"),
+            resultTable = $(".table-responsive");
+
+    public AutomationPracticeFormPage openPage() {
+        open("/automation-practice-form");
+        return this;
+    }
+
+
+    public AutomationPracticeFormPage RemoveBanner(){
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        return this;
+    }
+
+    public AutomationPracticeFormPage setFirstName(String value) {
+        firstNameInput.setValue(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setLastName(String value) {
+        lastNameInput.setValue(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setUserEmail(String value) {
+        userEmailInput.setValue(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setGender(String value) {
+        gender.$(byText(value)).click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage setUserNumber(String value) {
+        userNumberInput.setValue(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setBirthDay(String day, String month, String year) {
+        birthDayInput.click();
+        calendarComponent.setDate(day, month, year);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setSubject(String value, String subject) {
+        subjectValueInput.setValue(value);
+        subjectChosen.$(byText(subject)).click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage setHobby(String value) {
+        hobbyInput.$(byText(value)).click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage uploadPicture(String value) {
+        uploadPictureInput.uploadFromClasspath(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setAddress(String value) {
+        addressInput.setValue(value);
+        return this;
+    }
+
+    public AutomationPracticeFormPage setState(String value) {
+        stateInput.click();
+        stateChosen.$(byText(value)).click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage setCity(String value) {
+        cityInput.click();
+        cityChosen.$(byText(value)).click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage pushSubmitButton() {
+        submitButton.click();
+        return this;
+    }
+
+    public AutomationPracticeFormPage verifyResult(String key, String value) {
+        resultTable.$(byText(key)).parent().shouldHave(text(value));
+        return this;
+    }
+}
---
+++ src/test/java/com/demoqa/tests/AutomationPracticeFormWithPageObjects.java
@@ -0,0 +1,41 @@
+package com.demoqa.tests;
+
+import com.demoqa.pages.AutomationPracticeFormPage;
+import org.junit.jupiter.api.Test;
+
+public class AutomationPracticeFormWithPageObjects extends TestBase {
+
+    AutomationPracticeFormPage automationPracticeFormPage = new AutomationPracticeFormPage();
+
+    @Test
+    void formValidation() {
+        automationPracticeFormPage
+                .openPage()
+                .RemoveBanner()
+                .setFirstName("Alex")
+                .setLastName("Egorov")
+                .setUserEmail("AlexEgorov@mail.ru")
+                .setGender("Male")
+                .setUserNumber("9991112233")
+                .setBirthDay("20", "April", "1994")
+                .setSubject("g", "English")
+                .setSubject("m", "Maths")
+                .setHobby("Sports")
+                .uploadPicture("images.jpg")
+                .setAddress("Address №1")
+                .setState("NCR")
+                .setCity("Delhi")
+                .pushSubmitButton()
+
+                .verifyResult("Student Name", "Alex Egorov")
+                .verifyResult("Student Email", "AlexEgorov@mail.ru")
+                .verifyResult("Gender", "Male")
+                .verifyResult("Mobile", "9991112233")
+                .verifyResult("Date of Birth", "20 April,1994")
+                .verifyResult("Subjects", "English, Maths")
+                .verifyResult("Hobbies", "Sports")
+                .verifyResult("Picture", "images.jpg")
+                .verifyResult("Address", "Address №1")
+                .verifyResult("State and City", "NCR Delhi");
+    }
+}
